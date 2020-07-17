8ca447eb65dbbe2b4e69adcae96f89e3a1f09831
lennypeers <lenny.peers@gmail.com>
2020-07-17 22:19:35 +0200

    Removed color blocks. -> Bicolor theme

--- night-theme/polybar/scripts/temp.sh
+++ night-theme/polybar/scripts/temp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-	echo " $(cat /sys/devices/platform/applesmc.768//temp10_input| sed 's/000//')°C"
+	echo " $(cat /sys/devices/platform/applesmc.768//temp10_input| sed 's/000//')°C"
 	sleep 10
 done
