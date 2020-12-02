9b8dc500f1b79b7a7d9e121816a0d9a6bfa4631f
Ny Aina Pedersen <ny-aina.pedersen@etu.univ-grenoble-alpes.fr>
2020-12-02 15:13:19 +0100

    update

---
+++ scripts/fan.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+toggle() {
+	local tmp
+	[[ $(cat /sys/devices/platform/applesmc.768/fan1_manual) = 1 ]] && tmp=0
+	echo ${tmp:-1} | tee /sys/devices/platform/applesmc.768/fan1_manual 2>/dev/null 1>&2
+
+}
+
+case $1 in
+	toggle)
+		toggle
+		;;
+	"")
+		echo usage: fan \<toggle\|speed\> 1>&2
+		exit 1
+		;;
+	*)
+		echo $1 | tee /sys/devices/platform/applesmc.768/fan1_output
+		;;
+esac
