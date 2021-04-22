c3bb7f0d1e9a383c759ec484a0ea71bfc51806e0
lennypeers <lenny.peers@gmail.com>
2021-04-22 14:28:14 +0200

    update screencast
    - add options
        - help
        - sound
        - force
        - custom file name
    - now uses notification

--- scripts/scast.sh
+++ scripts/scast.sh
@@ -1,22 +1,71 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# kill other instances
+function notify() {
+	notify-send "Screencast" "$1"
+	exit ${2:-255}
+}
+
+# kill other instances of ffmpeg
 a="$(pidof ffmpeg)"
-if [ -n "$a" ]; then
+if [[ -n "$a" ]]; then
 	kill $a
-	echo Screencast stopped
-	exit 0
+	notify "Screencast stopped" 0
 fi
 
 cd ~/Videos
 name=$(date "+%Y-%m-%d-%H.%M.%S-screencast.mp4")
+dimensions=$(xdpyinfo | awk '/dimensions/{print $2}')
 
-if [ "$1" = "-s" ]; then # with sound
-	ffmpeg -f x11grab -video_size 1440x900 -framerate 60 -i $DISPLAY -f pulse -i alsa_input.pci-0000_00_1b.0.analog-stereo -ac 1 -c:v libx264 -preset ultrafast -c:a aac $name >/dev/null 2>&1 &
-	echo Screencast with sound
-else # no sound
-	ffmpeg -f x11grab -video_size 1440x900 -framerate 60 -i $DISPLAY -c:v libx264 -preset ultrafast -c:a aac $name >/dev/null 2>&1 &
-	echo Screencast with no sound
-fi
+function help() {
+	cat <<EOF
+usage: ./${0##*/} [-hfs] [filename]
+	-h --help   show help
+	-f --force  overwrite output file
+	-s --sound  record with sound
+EOF
+}
 
+while [[ -n "$1" ]]; do
+	case "$1" in
+		-h | --help)
+			help
+			exit 255
+			;;
+
+		-s | --sound) # with sound
+		flags+=" -f pulse"
+		flags+=" -i alsa_input.pci-0000_00_1b.0.analog-stereo"
+		flags+=" -ac 1"
+		;;
+
+		-f | --force) # overwrite
+		force=" -y"
+		;;
+
+		*) # a filename ?
+		[[ -n "$custom_name" ]] && \
+			notify "Cannot parse the argument: $1"
+		custom_name=1
+		name="$1"
+		;;
+	esac
+	shift
+done
+
+[[ -f "$name" && -z "$force" ]] && \
+	notify "File exists" 254
+
+ffmpeg \
+	-framerate 60 \
+	-f x11grab \
+	-video_size $dimensions \
+	-thread_queue_size 1024 \
+	-i $DISPLAY \
+	$flags \
+	-c:v libx264 \
+	-preset ultrafast \
+	-c:a aac \
+	$force \
+	"$name" >/dev/null 2>&1 &
 disown
+notify "Screencast started" 0
