d2da0d189f81c0f50108d74b89aa52e5e10b95d7
lennypeers <lenny.peers@gmail.com>
2021-05-14 18:39:02 +0200

    fix multihead

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,9 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -c ~/.config/polybar/config.ini laptop &
+monitors=($(bspc query -M --names | sort))
+# LVDS-1 is indexed 1. Gotta reverse it
+[[ ${monitors[0]} == HDMI-1 ]] && monitors=($(bspc query -M --names | sort -r))
+
+MONITOR="${monitors[0]}" polybar -c ~/.config/polybar/config.ini laptop &
 polybar -c ~/.config/polybar/config.ini right &
--- scripts/dock.sh
+++ scripts/dock.sh
@@ -4,12 +4,37 @@
 # lennypeers
 
 monitors=($(bspc query -M --names | sort))
+# LVDS-1 is indexed 1. Gotta reverse it
+[[ ${monitors[0]} == HDMI-1 ]] && monitors=($(bspc query -M --names | sort -r))
 
-if [[ -z "$1" ]]; then
-  xrandr --output "${monitors[0]}" --off --output "${monitors[1]:-HDMI-1}" --auto --primary --delmonitor "${monitors[0]}" &>/dev/null
-else
-  xrandr --output "${monitors[0]}" --auto --primary --output "${monitors[1]:-HDMI-}" --auto --right-of "${monitors[0]}" &>/dev/null
-fi
+
+usage() {
+    cat << _HELPTXT
+usage: ${0##*/} [--help | -h] [--dock | -d] [--multi | -m]
+_HELPTXT
+}
+
+case "$1" in
+  -h | --help)
+    usage
+    exit 0
+  ;;
+
+  -d | --dock)
+  # laptop monitor off (dock)
+    xrandr --output "${monitors[0]}" --off --output "${monitors[1]:-HDMI-1}" --auto --primary --delmonitor "${monitors[0]}" &>/dev/null
+  ;;
+
+  -m | --multi)
+  # multi head
+    xrandr --output "${monitors[0]}" --auto --primary --output "${monitors[1]:-HDMI-1}" --auto --right-of "${monitors[0]}" &>/dev/null
+  ;;
+
+  *)
+    usage
+    exit 255
+  ;;
+esac
 
 THEME=$(< ~/.t)
 ~/dotfiles/polybar/launch.sh &>/dev/null
