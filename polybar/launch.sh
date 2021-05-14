ae1ca75d693c7242b759f92c444aa9eca10902c0
lennypeers <lenny.peers@gmail.com>
2021-05-14 18:49:47 +0200

    fix pt3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,5 +8,6 @@ monitors=($(bspc query -M --names | sort))
 # LVDS-1 is indexed 1. Gotta reverse it
 [[ ${monitors[0]} == HDMI-1 ]] && monitors=($(bspc query -M --names | sort -r))
 
-MONITOR="${monitors[0]}" polybar -c ~/.config/polybar/config.ini laptop &
+[[ "${monitors[0]}" != HDMI-1 ]] && \
+	MONITOR="${monitors[0]}" polybar -c ~/.config/polybar/config.ini laptop &
 polybar -c ~/.config/polybar/config.ini right &
