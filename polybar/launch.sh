8360ef5bcdc3d75c6454159143edecc98cb44f4d
lennypeers <lenny.peers@gmail.com>
2022-01-02 22:11:22 +0100

    eww: misc update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-pan_cmd="eww windows | grep \* && eww close-all || eww open panel_right"
+readonly pan_cmd="eww windows | grep \* && eww close-all || eww open panel_right || eww open panel_left"
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR=HDMI-1 PAN="$pan_cmd" polybar -c ~/.config/polybar/config.ini right  &
-MONITOR=eDP-1  polybar -c ~/.config/polybar/config.ini laptop &
-MONITOR=LVDS-1 polybar -c ~/.config/polybar/config.ini laptop &
+monitors=($(xrandr --listactivemonitors | awk '{print $4}'))
+
+_() { # $1 in monitors?
+  case "${monitors[@]}" in *"$1"*);; *) return 1;; esac
+}
+
+_ HDMI-1 && \
+  MONITOR=HDMI-1 PAN_CMD="$pan_cmd" polybar -c ~/.config/polybar/config.ini right  &
+_ eDP-1 && \
+  MONITOR=eDP-1  polybar -c ~/.config/polybar/config.ini laptop &
+_ LVDS-1 && \
+  MONITOR=LVDS-1 polybar -c ~/.config/polybar/config.ini laptop &
 
 disown -a
