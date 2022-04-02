48e80f096eeb48065960e9247056c330501fa2b9
lennypeers <lenny.peers@gmail.com>
2022-04-02 23:58:55 +0200

    Simplify code

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-readonly pan_cmd="eww windows | grep \* && eww close-all || eww open panel_right || eww open panel_left"
 monitors=($(xrandr --listactivemonitors | awk '{print $4}'))
 
-killall polybar
+killall -q polybar
 
-_() { # $1 in monitors?
+on() { # $1 in monitors?
   case "${monitors[@]}" in *"$1"*);; *) return 1;; esac
 }
 
-_ HDMI-1 && \
-  MONITOR=HDMI-1 PAN_CMD="$pan_cmd" polybar -c ~/.config/polybar/config.ini right  &
-_ eDP-1 && \
-  MONITOR=eDP-1  polybar -c ~/.config/polybar/config.ini laptop &
-_ LVDS-1 && \
-  MONITOR=LVDS-1 polybar -c ~/.config/polybar/config.ini laptop &
+on HDMI-1 && \
+  MONITOR=HDMI-1 polybar -r -c ~/.config/polybar/config.ini right  &
+on eDP-1 && \
+  MONITOR=eDP-1  polybar -r -c ~/.config/polybar/config.ini laptop &
+on LVDS-1 && \
+  MONITOR=LVDS-1 polybar -r -c ~/.config/polybar/config.ini laptop &
 
 disown -a
 
