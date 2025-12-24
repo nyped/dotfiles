4b75b42c2b48813d6cc93de615ba53f66bb924a9
nyped <lenny.peers@gmail.com>
2025-12-24 18:10:04 +0100

    nvim: update remap

--- nvim/.config/nvim/lua/base/remap.lua
+++ nvim/.config/nvim/lua/base/remap.lua
@@ -10,17 +10,13 @@ vim.keymap.set("t", "<C-w>w", [[<C-\><C-n><C-w>w]])
 -- don't paste in normal mode
 vim.keymap.set("n", "<MiddleMouse>", "<Nop>")
 
--- bad habits
-vim.keymap.set("n", "<BS>", "g<Tab>")
-
 -- add lines
 vim.keymap.set("n", "O", "O<Esc>")
 vim.keymap.set("n", "o", "o<Esc>")
 
--- switching tabs with <Tab> while keeping <C-I>
-vim.keymap.set("n", "<C-I>", "<C-I>")
-vim.keymap.set("n", "<S-Tab>", "gT")
-vim.keymap.set("n", "<Tab>", "gt")
+-- switching tabs with backspace
+vim.keymap.set("n", "<BS>", "gt")
+vim.keymap.set("n", "<S-BS>", "gT")
 
 -- switch windows
 for i = 1, 9 do
@@ -64,37 +60,3 @@ end, {})
 
 -- wrap toggle
 vim.keymap.set("n", "<Leader>gw", "<cmd>set wrap!<CR>", { desc = "Wrap text" })
-
--- template {{{
-vim.api.nvim_create_user_command("Tpl", function(opts)
-  local empty_buf = vim.fn.line("$") == 1
-    and vim.api.nvim_get_current_line():len() == 0
-  local prefix = ""
-
-  if opts.range ~= 0 then
-    prefix = [['<.'>]]
-  elseif opts.bang or empty_buf then
-    prefix = "%"
-  end
-
-  vim.cmd(prefix .. "!tpl " .. tostring(opts.args))
-end, {
-  bang = true,
-  desc = "Tpl: template creator",
-  nargs = 1,
-  range = true,
-  complete = function(cmd)
-    local ret = {}
-    local tmp_ft =
-      vim.split(vim.fn.system({ "tpl", "--list" }), "\n", { trimempty = true })
-
-    for _, value in ipairs(tmp_ft) do
-      if string.find(value, cmd) then
-        table.insert(ret, value)
-      end
-    end
-
-    return ret
-  end,
-})
--- }}}
